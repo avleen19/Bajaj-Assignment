3e43a5f3f53f99a125c2a48e88b5d38a864b54f8
AvleenKaur <avleenkaur1904@gmail.com>
2025-07-29 20:33:47 +0530

    commit2

--- app.js
+++ app.js
@@ -52,5 +52,5 @@ app.post("/bfhl", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
